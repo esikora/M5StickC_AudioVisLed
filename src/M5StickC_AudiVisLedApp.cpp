cb73c5523458aaf7fe1ee6bfbff18cb8e4863683
Ernst Sikora <ernst@sikora-online.de>
2021-08-16 07:40:42 +0200

    Update M5StickC_AudiVisLedApp.cpp
    Added licence and copyright information

--- src/M5StickC_AudiVisLedApp.cpp
+++ src/M5StickC_AudiVisLedApp.cpp
@@ -1,3 +1,25 @@
+/**
+    M5StickC_AudioVisLedApp:
+    This application has been developed to use an M5StickC device (ESP32)
+    as an audio sampling and visualization device. It samples audio data
+    from the built-in microphone using i2s. The sampled data is transformed
+    into the frequency domain using the arduinoFFT library.
+    Copyright (C) 2021 by Ernst Sikora
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Arduino.h>
 
 #include <M5StickC.h>
